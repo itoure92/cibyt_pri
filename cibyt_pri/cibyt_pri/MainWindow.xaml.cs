a308b0fc466a5b98a631c21ad6623cd45338ade4
Dan Azoulay <daloulou@hotmail.fr>
2017-12-03 14:32:23 +0100

    [ADD] - First commit - adding all files

---
+++ cibyt_pri/cibyt_pri/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cibyt_pri
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Boolean isclicked = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string key = "sbShowLeftMenu";
+            if (isclicked)
+            {
+                key = "sbShowLeftMenu";
+                isclicked = false;
+            }
+            else
+            {
+                key = "sbHideLeftMenu";
+                isclicked = true;
+            }
+            Storyboard sb = Resources[key] as Storyboard;
+            sb.Begin(this.st_panel);
+        }
+    }
+}
