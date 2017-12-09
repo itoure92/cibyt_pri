4ddd6db8b25a077be7a2161519a2d4058047ff69
Dan Azoulay <daloulou@hotmail.fr>
2017-12-09 13:31:46 +0100

    [UPDATE] - including the profile setting page

--- cibyt_pri/cibyt_pri/MainWindow.xaml.cs
+++ cibyt_pri/cibyt_pri/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cibyt_pri.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -47,6 +48,11 @@ namespace cibyt_pri
         private void Home_Button_Click(object sender, RoutedEventArgs e)
         {
             this.Main.Content = new HomePage();
+        }
+
+        private void Profile_setting_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Main.Content = new ProfileSettingPage();
         }
     }
 }
