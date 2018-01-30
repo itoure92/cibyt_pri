562986a65887400f1596dcbdb63fa832e22795e4
Dan Azoulay <daloulou@hotmail.fr>
2018-01-30 21:10:18 +0100

    [UPDATE-ADD] DataModel

--- cibyt_pri/cibyt_pri/DataModel/ApplicationPage.cs
+++ cibyt_pri/cibyt_pri/DataModel/ApplicationPage.cs
@@ -1,13 +1,23 @@
-﻿namespace cibyt_pri
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace cibyt_pri.DataModel
 {
-    /// <summary>
-    /// A page of the application
-    /// </summary>
-    public enum ApplicationPage
+    class ApplicationPage
     {
-        /// <summary>
-        /// Login page
-        /// </summary>
-        Login = 0,
+        public Window mWindow;
+        public PageType pageType;
+
+        public ApplicationPage(Window window, PageType pageType)
+        {
+            this.mWindow = window;
+            this.pageType = pageType;
+        }
     }
+
+    
 }
---
+++ cibyt_pri/cibyt_pri/DataModel/PageType.cs
@@ -0,0 +1,18 @@
+﻿namespace cibyt_pri
+{
+    /// <summary>
+    /// A page of the application
+    /// </summary>
+    public enum PageType
+    {
+        /// <summary>
+        /// Login page
+        /// </summary>
+        Login = 0,
+        HomePage = 1,
+        ProfilSettingPage = 2,
+        GameSettingPage = 3,
+        NewUser = 4,
+        ForgetPasswordPage = 5,
+    }
+}
