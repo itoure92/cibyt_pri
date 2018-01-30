d7a6c6c840018b3fec97bb25b33c85f628ee53d3
Dan Azoulay <daloulou@hotmail.fr>
2018-01-30 21:08:31 +0100

    [UPDATE-ADD] pages

--- cibyt_pri/cibyt_pri/Pages/LoginPage.xaml.cs
+++ cibyt_pri/cibyt_pri/Pages/LoginPage.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace cibyt_pri
 {
@@ -23,6 +12,40 @@ namespace cibyt_pri
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void sign_in_Click(object sender, RoutedEventArgs e)
+        {
+            if (verifyId_Password(this.login.Text, this.password.Password))
+            {
+                ((WindowViewModel)WindowViewModel.GetWindow().DataContext).CurrentPage = PageType.HomePage;
+                ((WindowViewModel)WindowViewModel.GetWindow().DataContext).OnPropertyChanged("CurrentPage");
+            }
+            else
+                MessageBox.Show("Uncorrect Login or Password - Try again");
+            
+        }
+
+        //Verify the login and password of the user - use the database
+        private Boolean verifyId_Password(string id, string password)
+        {
+            if (id.Equals("ibrah") && password.Equals("toure"))
+                return true;
+            return false;
+        }
+
+        private void sign_up_Click(object sender, RoutedEventArgs e)
+        {
+            ((WindowViewModel)WindowViewModel.GetWindow().DataContext).CurrentPage = PageType.NewUser;
+            ((WindowViewModel)WindowViewModel.GetWindow().DataContext).OnPropertyChanged("CurrentPage");
+        }
+
+        //Ask for the mail address of the user and send him an email with a random password
+        //He has to reconnect with the random password and has to change his old password
+        private void Forget_password_Click(object sender, RoutedEventArgs e)
+        {
+            ((WindowViewModel)WindowViewModel.GetWindow().DataContext).CurrentPage = PageType.ForgetPasswordPage;
+            ((WindowViewModel)WindowViewModel.GetWindow().DataContext).OnPropertyChanged("CurrentPage");
         }
     }
 }
---
+++ cibyt_pri/cibyt_pri/Pages/NewUserPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cibyt_pri.Pages
+{
+    /// <summary>
+    /// Logique d'interaction pour NewUserPage.xaml
+    /// </summary>
+    public partial class NewUserPage : Page
+    {
+        public NewUserPage()
+        {
+            InitializeComponent();
+        }
+
+        //Create a new account for a user - Store all information to the DB
+        private void Create_Account_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
